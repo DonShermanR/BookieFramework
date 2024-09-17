29800f0464c25f7d9fa14c383343d67595a2d535
Germancho <edurodriguez85@yahoo.es>
2024-09-16 21:46:38 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BOOKIE'
+copyright = '2024, Germán'
+author = 'Germán Rodríguez-Galán'
 
 release = '0.1'
 version = '0.1.0'
